00b07078969ba40229db13aa513eab56b4c32994
arik <arik@bambidynamic.com>
2021-02-14 18:09:38 +0200

    first queue example

--- parallel_example.py
+++ parallel_example.py
@@ -1,29 +1,17 @@
-# Parallelizing using Pool.apply()
+from multiprocessing import Queue
 
-import multiprocessing as mp
-def howmany_within_range(row, minimum, maximum):
-    """Returns how many numbers lie within `maximum` and `minimum` in a given `row`"""
-    count = 0
-    for n in row:
-        if minimum <= n <= maximum:
-            count = count + 1
-    return count
-import numpy as np
-from time import time
+colors = ['red', 'green', 'blue', 'black']
+cnt = 1
+# instantiating a queue object
+queue = Queue()
+print('pushing items to queue:')
+for color in colors:
+    print('item no: ', cnt, ' ', color)
+    queue.put(color)
+    cnt += 1
 
-# Prepare data
-np.random.RandomState(100)
-arr = np.random.randint(0, 10, size=[200000, 5])
-data = arr.tolist()
-data[:5]
-# Step 1: Init multiprocessing.Pool()
-# pool = mp.Pool(mp.cpu_count())
-# # print(pool)
-# # Step 2: `pool.apply` the `howmany_within_range()`
-# results = [pool.apply(howmany_within_range, args=(row, 4, 8)) for row in data]
-#
-# # Step 3: Don't forget to close
-# pool.close()
-#
-# print(results[:10])
-#> [3, 1, 4, 4, 4, 2, 1, 1, 3, 3]
+print('\npopping items from queue:')
+cnt = 0
+while not queue.empty():
+    print('item no: ', cnt, ' ', queue.get())
+    cnt += 1
